81d0804b00ede430cba63bc456af60c0d2a1d7ea
amirhossein <79211332+capamir@users.noreply.github.com>
2024-01-13 19:12:52 +0330

    second assignment

---
+++ week2/add-matrices/src/index.js
@@ -0,0 +1,5 @@
+function addMatrices(matrix1, matrix2) {
+  // your code...
+}
+
+module.exports = addMatrices;
--- week2/reminder/src/main.js
+++ week2/reminder/src/main.js
@@ -1,8 +1,26 @@
 // use this tasks to test your code
-const tasks = require('./data.json');
+const tasks = require("./data.json");
 
-function reminder (data, currentDate) {
-   // YOUR CODE HERE
+function reminder(data, currentDate) {
+  // YOUR CODE HERE
+  let output = [];
+  const currentTime = new Date(data);
+  currentDate.forEach((task) => {
+    let message = "";
+    const taskTime = new Date(task.date);
+    const timeDifference = taskTime - currentTime;
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    if (hours >= 3) {
+      message = `wait fot it : ${task.title}`;
+    } else if (hours >= 0) {
+      message = `just do it : ${task.title}`;
+    } else {
+      message = `too late : ${task.title}`;
+    }
+
+    output.push(message);
+  });
+  return output;
 }
 
-module.exports = reminder
+module.exports = reminder;
