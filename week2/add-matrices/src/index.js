f8cb19ad1d26bc478811a43a2b58ebe1755bd897
amirhossein <79211332+capamir@users.noreply.github.com>
2024-01-13 19:37:39 +0330

    third assignment completed

--- week2/add-matrices/src/index.js
+++ week2/add-matrices/src/index.js
@@ -1,5 +1,14 @@
 function addMatrices(matrix1, matrix2) {
   // your code...
+  const resultMatrix = Array.from({ length: 4 }, () => Array(4).fill(0));
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+  }
+
+  return resultMatrix;
 }
 
 module.exports = addMatrices;
--- week2/reminder/src/main.js
+++ week2/reminder/src/main.js
@@ -1,26 +1,30 @@
 // use this tasks to test your code
-const tasks = require("./data.json");
+// const tasks = require("./data.json");
 
 function reminder(data, currentDate) {
   // YOUR CODE HERE
-  let output = [];
   const currentTime = new Date(data);
-  currentDate.forEach((task) => {
-    let message = "";
-    const taskTime = new Date(task.date);
-    const timeDifference = taskTime - currentTime;
-    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-    if (hours >= 3) {
-      message = `wait fot it : ${task.title}`;
-    } else if (hours >= 0) {
-      message = `just do it : ${task.title}`;
-    } else {
-      message = `too late : ${task.title}`;
-    }
+  let message = "";
+  const taskTime = new Date(currentDate.date);
+  const timeDifference = taskTime - currentTime;
+  const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+  if (hours >= 3) {
+    message = `wait fot it : ${currentDate.title}`;
+  } else if (hours >= 0) {
+    message = `just do it : ${currentDate.title}`;
+  } else {
+    message = `too late : ${currentDate.title}`;
+  }
 
-    output.push(message);
-  });
-  return output;
+  return message;
 }
+
+const result = reminder("2024-01-15T18:00:00Z", {
+  id: 1,
+  title: "buy for breakfast",
+  description: "buy a bread for breakfast",
+  date: "2024-01-15T20:00:00Z",
+});
+console.log(result);
 
 module.exports = reminder;
