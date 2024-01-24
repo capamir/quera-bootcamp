b435380eb308cf7007cc8bb7f034800ae74c1cbc
amirhossein <79211332+capamir@users.noreply.github.com>
2024-01-24 18:53:40 +0330

    challenge 9 completed

--- week3/09-filterList/src/main.js
+++ week3/09-filterList/src/main.js
@@ -1,8 +1,28 @@
 // you can use this data to test your function
 const list = require("./data.json");
 
-function filterList(data, model_numbers) {
- 
+function filterList(data, model_numbers = []) {
+  const result = [];
+
+  const brands = Object.keys(data);
+  // Iterate through each brand in the data
+  brands.forEach((brand) => {
+    // Check if the brand has an array of cars
+    if (Array.isArray(data[brand])) {
+      // Filter the cars based on the model_numbers
+      const filteredCars = data[brand].filter((car) =>
+        model_numbers.includes(car.model_number)
+      );
+
+      // Append the filtered cars to the result array
+      result.push(...filteredCars);
+    }
+  });
+
+  return result.length === 0 ? "not found!" : result;
 }
+
+res = filterList(list, ["A6F512", "R754F"]);
+console.log(res);
 
 module.exports = filterList;
