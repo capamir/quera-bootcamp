2c5c2dff360d29c097c8cafd5e1e9f53331751fb
amirhossein <79211332+capamir@users.noreply.github.com>
2024-01-29 11:54:37 +0330

    task list challenge completed

--- week4/03-taskList/main.js
+++ week4/03-taskList/main.js
@@ -6,9 +6,11 @@ const deleteButton = document.getElementById("delete");
 const error = document.getElementById("errorContainer");
 
 // Task item scheme:
-{/* <li class="todos__todo">
+{
+  /* <li class="todos__todo">
 ${newTask} <img src="../assets/trash.svg" class="todos__todo--trash" id="delete" alt="trash" />
-</li> */}
+</li> */
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,11 +27,26 @@ form.addEventListener("submit", (e) => {
   }
 
   // TODO: Add task to taskList
-
+  taskList.innerHTML += ` <li class="todos__todo">
+  ${newTask} <img src="./assets/trash.svg" class="todos__todo--trash" id="delete" alt="trash" />
+</li>`;
   // TODO: Reset form input
+  input.value = "";
 });
 
 taskList.addEventListener("click", (e) => {
+  const targetElement = e.target;
+
   // TODO: Complete task when clicked on task item
+  // Check if the clicked element is a task item
+  if (targetElement.classList.contains("todos__todo")) {
+    targetElement.style.setProperty("background-color", "#c9c8c8");
+    targetElement.style.setProperty("text-decoration", "line-through");
+  }
+
   // TODO: Delete task item
+  // Check if the clicked element is the delete (trash) icon
+  if (targetElement.classList.contains("todos__todo--trash")) {
+    targetElement.parentElement.remove();
+  }
 });
